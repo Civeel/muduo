2aab0a6b6287b55511a9022fdd6c9b18ae263fc6
Civeel <543768671@qq.com>
2025-03-18 17:15:19 +0800

    bash

--- autobuild.sh
+++ autobuild.sh
@@ -3,13 +3,13 @@
 set -e
 
 # 如果没有build目录，创建该目录
-if [ ! -d 'pwd'/build ]; then
-    mkdir 'pwd'/build
+if [ ! -d `pwd`/build ]; then
+    mkdir `pwd`/build
 fi
 
-rm -rf 'pwd'/build/*
+rm -rf `pwd`/build/*
 
-cd 'pwd'/build &&
+cd `pwd`/build &&
     cmake .. &&
     make
 
@@ -25,6 +25,6 @@ do
     cp $header /usr/include/mymuduo
 done
 
-cp 'pwd'/lib/libmymuduo.so /usr/lib
+cp `pwd`/lib/libmymuduo.so /usr/lib
 
 ldconfig
